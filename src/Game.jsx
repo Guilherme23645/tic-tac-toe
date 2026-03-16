043b3b7b8513b53a860902107b174c67f2900695
Guilherme Henrique de Paula <gui@debian.devvm.com>
2026-03-16 18:56:07 -0300

    feat: create timeline with array representing differents states of the board

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,8 +1,24 @@
 import Board from "./Board"
+import { useState } from "react"
 
 const Game = () => {
+  const [flag, setFlag] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  const handlePlay = () => {
+    // TODO
+  }
+
   return (
-    <Board />
+    <div className="game">
+      <div className="game-board">
+        <Board flag={flag} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
   )
 }
 
